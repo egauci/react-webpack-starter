2b7c47444df7ed0caaf389c45a2be07bc8802106
Ed Gauci <ed@gauci.co>
2016-04-13 10:51:49 -0700

    another fact button

--- src/components/numberfact/Numberfact.js
+++ src/components/numberfact/Numberfact.js
@@ -2,14 +2,25 @@ import React, {Component, PropTypes} from 'react';
 import lcss from './Numberfact.css';
 
 class Numberfact extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
   static propTypes = {
     clearFact: PropTypes.func.isRequired,
     fact: PropTypes.object.isRequired,
     getFact: PropTypes.func.isRequired
   }
-  componentWillMount() {
+  getAFact() {
     this.props.clearFact();     // clear old fact if any
     this.props.getFact();       // fetch a new fact
+  }
+  componentWillMount() {
+    this.getAFact();
+  }
+  handleClick(e) {
+    e.preventDefault();
+    this.getAFact();
   }
   render() {
     const number = this.props.fact.get('number');
@@ -22,6 +33,7 @@ class Numberfact extends Component {
         <h2>Random Number Fact</h2>
         <p><span className={lcss.label}>Number:</span> {number}</p>
         <p><span className={lcss.label}>Fact:</span> {text}</p>
+        <button className={lcss.button} onClick={this.handleClick}>Get another fact</button>
       </div>
     );
   }
