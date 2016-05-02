7124c0068b8127f0096204b949e075b515bbe7fc
Ed Gauci <ed@gauci.co>
2016-05-02 12:11:12 -0700

    Handle numberfact fetch failure

--- src/reducers/fact.js
+++ src/reducers/fact.js
@@ -5,6 +5,15 @@ const initState = Immutable.Map({});
 const fact = (state = initState, action) => {
   switch (action.type) {
   case 'GET_FACT':
+    if (action.error) {
+      let text = 'Failed to get a number fact';
+      if (action.payload.data && action.payload.data.message) {
+        text = `Error: ${action.payload.data.message}`;
+      } else if (action.payload.statusText) {
+        text = `Error: ${action.payload.status} ${action.payload.statusText}`;
+      }
+      return Immutable.Map({number: '', text});
+    }
     if (action.payload && action.payload.data) {
       return Immutable.Map(action.payload.data);
     }
